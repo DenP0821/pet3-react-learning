368d2b79593d7e71e0e2c595241bd5f406ed353a
Denis P <16_denis@mail.ru>
2025-12-08 03:59:59 +0300

    Deploy project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/pet3-react-learning/' : '/',
+  base: "/pet3-react-learning",
   plugins: [react()],
-})
+});
